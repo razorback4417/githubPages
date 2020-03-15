2d55c41970f84229c3a84bc8334a2e0a799482df
razorback4417 <44447723+razorback4417@users.noreply.github.com>
2020-03-14 21:49:43 -0400

    Update TODO(updated).js

--- TODO(updated).js
+++ TODO(updated).js
@@ -1,36 +1,46 @@
-import React from 'react';
-import { View, Button, Text, ScrollView, StyleSheet, Switch } from 'react-native';
-import { Constants } from 'expo';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  Switch,
+  ScrollView,
+} from 'react-native';
 
 let id = 0;
 
 const Todo = props => (
-  <View style={styles.TodoContainer}>
+  <View style={styles.container}>
     <Switch value={props.todo.checked} onValueChange={props.onToggle} />
-    <Button onPress={props.onDelete} title="delete" />
     <Text>{props.todo.text}</Text>
   </View>
 );
 
 export default class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      TextInputValue: '',
       todos: [],
     };
   }
 
+  buttonClickListener = () => {
+    const { TextInputValue } = this.state;
+    //Alert.alert(TextInputValue);
+    this.addTodo();
+    console.log(this.state.todos);
+  };
+
   addTodo() {
     id++;
-    const text = `TODO number ${id}`;
+    const text = `${this.state.TextInputValue}`
     this.setState({
       todos: [...this.state.todos, { id: id, text: text, checked: false }],
-    });
-  }
-
-  removeTodo(id) {
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id),
     });
   }
 
@@ -46,16 +56,43 @@ export default class App extends React.Component {
       }),
     });
   }
+
   render() {
     return (
-      <View style={[styles.appContainer, styles.fill]}>
-        <Text>To-do count: {this.state.todos.length}</Text>
-        <Text>
-          Unchecked to-do count:{' '}
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Todo List</Text>
+
+        <TextInput
+          style={{
+            height: 45,
+            width: '95%',
+            borderColor: 'gray',
+            borderWidth: 2,
+          }}
+          placeholder="   Enter your  'Todo'  item"
+          onChangeText={TextInputValue => this.setState({ TextInputValue })}
+          underlineColorAndroid="transparent"
+        />
+
+        <View style={{ width: '80%', margin: 15 }}>
+          <Button
+            onPress={this.buttonClickListener}
+            title="Add Todo"
+            color="#00B0FF"
+          />
+        </View>
+
+        <Text style={styles.words}>
+          {'   '}
+          Todo count: {this.state.todos.length}{' '}
+        </Text>
+        <Text style={styles.words}>
+          {'   '}
+          Unchecked Todo count:{' '}
           {this.state.todos.filter(todo => !todo.checked).length}
         </Text>
-        <Button onPress={() => this.addTodo()} title="Add TODO" />
-        <ScrollView style={styles.fill}>
+
+        <ScrollView style={styles.containerTwo}>
           {this.state.todos.map(todo => (
             <Todo
               onToggle={() => this.toggleTodo(todo.id)}
@@ -70,14 +107,31 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  TodoContainer: {
-    flexDirection: 'row', 
-    alignItems: 'center'
-  },
-  appContainer: {
-    paddingTop: Constants.statusBarHeight
-  },
-  fill: {
+  container: {
     flex: 1,
-  }
-})
+    paddingTop: 40,
+    paddingLeft: 20,
+    paddingBottom: 40,
+    //alignItems: "center",
+    backgroundColor: '#f2f2f2',
+  },
+  containerTwo: {
+    flex: 1,
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingBottom: 10,
+    //alignItems: "center",
+    backgroundColor: '#f2f2f2',
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold',
+  },
+  words: {
+    fontSize: 18,
+    color: 'gray',
+    fontWeight: 'bold',
+  },
+});
