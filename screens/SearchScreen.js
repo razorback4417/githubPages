ba98b4b0447b5dc4282915b75b2da943c721b1fa
razorback4417 <44447723+razorback4417@users.noreply.github.com>
2020-05-31 11:19:52 -0400

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,68 +1,32 @@
-//import * as React from 'react';
-import React, { Component, useState } from 'react';
+import React from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { Constants } from 'expo';
+import axios from 'axios';
 
-import { Text, View, TextView, TextInput, StyleSheet, Button, Alert } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-
-//export default function RecordScreen() {//class RecordScreen extends React.Component {
-function RecordScreenFunction(input) {
-  //render() {
-  const [text, setText] = useState('');
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Record Here!</Text>
-      <TextInput
-        style={{ height: 40 }}
-        placeholder= "${input}"
-        onChangeText={(text) => setText(text)}
-        defaultValue={text}
-      />
-    </View>
-  );
-  //}
-}
-
-export default class RecordScreen extends React.Component {
+export default class SearchScreen extends React.Component {
   state = {
-    name: '',
-    type: '',
-    distance: 0,
-    time: 0,
+    search: '',
   };
+
+  static navigationOptions = ({ navigation }) => {
+    return { headerTitle: 'Search' };
+  };
+
+  handleSearch = search => {
+    this.setState({ search });
+    this.props.navigation.push('_map', this.state.search);
+  };
+
   render() {
     return (
-      <View>
+      <View styles={{ flex: 1 }}>
         <TextInput
           style={styles.input}
-          placeholder=" Name Of Run"
-          onChangeText={(value) => this.setState({ name: value })}
-          value={this.state.name}
+          placeholder="  Search..."
+          value={this.state.search}
+          onChangeText={(search) => this.setState({search})}
+          onSubmitEditing={this.handleSearch}
         />
-        <TextInput
-          style={styles.input}
-          placeholder=" Type"
-          onChangeText={(value) => this.setState({ type: value })}
-          value={this.state.type}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder=" Distance"
-          onChangeText={(value) => this.setState({ type: value })}
-          value={this.state.type}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder=" Time"
-          onChangeText={(value) => this.setState({ time: value })}
-          value={this.state.type}
-        />
-        <Button
-          title="Submit"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        />
-        
       </View>
     );
   }
