a90bd54addf8ca672593a872a638438e37d2f55d
razorback4417 <44447723+razorback4417@users.noreply.github.com>
2020-05-31 11:17:12 -0400

    Update FavoriteScreen.js

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,208 +1,102 @@
-import React from 'react';
-import {
-  Text,
-  View,
-  FlatList,
-  StyleSheet,
-  ScrollView,
-  Button,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
-import GLOBAL from '../global.js';
+//import * as React from 'react';
+import React, { Component, useState } from 'react';
 
+import { Text, View, TextView, TextInput, StyleSheet, Button, Alert, Constants, SafeAreaView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-import store from '../redux/store';
-import { addFavorite, deleteFavorite } from '../redux/actions';
-
-import { Alert } from 'react-native';
-
-function Squares(props) {
-  let bData = [];
-  for (var i = 0; i < GLOBAL.num; i++) {
-    bData.push(GLOBAL.businessLocations[i].name);
-  }
+import GLOBAL from '../global.js';
+//export default function RecordScreen() {//class RecordScreen extends React.Component {
+function RecordScreenFunction(input) {
+  //render() {
+  const [text, setText] = useState('');
   return (
-    <View style={{ height: 400, backgroundColor: props.color1 }}>
-      {bData.map((item, index) => {
-        return (
-          <TouchableOpacity
-            style={styles.btnContainer}
-            index={index}
-            onPress={event => {
-              Alert.alert('You added a favorite', `${item}`);
-              store.dispatch(addFavorite(item));
-              console.log(`store = ${JSON.stringify(store.getState())}`);
-            }}>
-            <Text>{item}</Text>
-          </TouchableOpacity>
-        );
-      })}
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Record Here!</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder= "${input}"
+        onChangeText={(text) => setText(text)}
+        defaultValue={text}
+      />
     </View>
   );
+  //}
 }
 
-function SquareFavorites(props) {
-  let favB = store.getState(); //[{ name: "Domenic's" }, { name: 'Knotty Pine' }]; //
-  // for (var i = 0; i < 3; i++) {
-  //   favB.push(store.getState().i);
-  // }
-  //console.log(`favB is ${JSON.stringify(favB)}`);
-  //console.log(`square favorites.... ${JSON.stringify(store.getState())}`)
-  return (
-    <View style={{ height: 400, backgroundColor: props.color1 }}>
-      {favB.map((item, index) => {
-        return (
-          <TouchableOpacity
-            style={styles.btnContainer}
-            onPress={event => {
-              Alert.alert(`You have deleted ${item}`);
-              console.log(`store b4 = ${JSON.stringify(store.getState())}`);
-              store.dispatch(deleteFavorite(item));
-              console.log(`store after= ${JSON.stringify(store.getState())}`);
-            }}>
-            <Text>{item}</Text>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
-  );
-}
-
-// function Squares(props) {
-//   var squares = [];
-//   for (var i = 0; i < props.numRows; i++) {
-//     squares.push(
-//       <View style={{ height: 50, backgroundColor: props.color1 }}>
-//         <TouchableOpacity
-//           style={styles.btnContainer}
-//           onPress={() => {
-//             store.dispatch(addFavorite({ name: GLOBAL.businessLocations[0].name }));
-//             alert('Pressed');
-//             console.log(GLOBAL.businessLocations)
-//             //console.log(store.getState());
-//           }}>
-//           <Ionicons name={'ios-add-circle-outline'} size={25} color={'#bbb'} />
-//           <Text>{`${i} ${GLOBAL.businessLocations[i].name}`}</Text>
-//         </TouchableOpacity>
-//       </View>
-//     );
-//   }
-//   return squares;
-// }
-
-// function SquareFavorites(props) {
-//   var squares = [];
-//   for (var i = 0; i < props.numRows; i++) {
-//     squares.push(
-//       <View style={{ height: 50, backgroundColor: props.color1 }}>
-//         <TouchableOpacity
-//           style={styles.btnContainer}
-//           onPress={() => {
-//             alert('Pressed');
-//           }}>
-//           <Ionicons
-//             name={'ios-close-circle-outline'}
-//             size={25}
-//             color={'#bbb'}
-//           />
-//           <Text>{`${store.getState().name}`}</Text>
-//         </TouchableOpacity>
-//       </View>
-//     );
-//   }
-//   return squares;
-// }
-
-export default class FavoriteScreen extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: GLOBAL.businessLocations,
-    };
-  }
-
-  // componentDidMount() {
-  //   console.log(GLOBAL.businessLocations)
-  // }
-
-  listEmptyComponent = () => {
-    return (
-      <View>
-        <Text style={{ textAlign: 'center', marginTop: 280 }}>
-          No Favorites
-        </Text>
-      </View>
-    );
+export default class RecordScreen extends React.Component {
+  state = {
+    name: '',
+    type: '',
+    distance: 0,
+    time: 0,
   };
 
+  handleSubmit = () => {
+    GLOBAL.gName = this.state.name
+    GLOBAL.gType = this.state.type
+    GLOBAL.gDistance = this.state.distance
+    GLOBAL.gTime = this.state.time
+    Alert.alert('Data has been submitted')
+  }
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'flex-start',
-          backgroundColor: 'yellow',
-        }}>
-        <View style={{ backgroundColor: '#bbb', flexDirection: 'row' }}>
-          <ScrollView
-            style={{
-              height: Dimensions.get('window').height / 2,
-              width: Dimensions.get('window').width,
-            }}
-            scrollEventThrottle={16}
-            ref={scrollView => {
-              this._leftView = scrollView;
-            }}
-            onScroll={e => {
-              if (!this.leftIsScrolling) {
-                this.rigthIsScrolling = true;
-                var scrollY = e.nativeEvent.contentOffset.y;
-                this._rightView.scrollTo({ y: scrollY });
-              }
-              this.leftIsScrolling = true;
-            }}>
-            <Squares numRows={GLOBAL.num} color1={'#50a2ef'} />
-          </ScrollView>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text> Record </Text>
+          <TextInput
+            style={styles.input}
+            placeholder=" Name Of Run"
+            onChangeText={(value) => this.setState({ name: value })}
+            value={this.state.name}
+            onBlur={() => console.log(`here is the state: ${this.state.name}`)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder=" Type"
+            onChangeText={(value) => this.setState({ type: value })}
+            value={this.state.type}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder=" Distance"
+            onChangeText={(value) => this.setState({ distance: value })}
+            value={this.state.distance}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder=" Time"
+            onChangeText={(value) => this.setState({ time: value })}
+            value={this.state.time}
+          />
+          <Button
+            style={{borderWidth: 20}}
+            title="Submit"
+            onPress={() => this.handleSubmit()}
+          />
+          
         </View>
-
-        <View style={{ backgroundColor: '#bbb', flexDirection: 'row' }}>
-          <ScrollView
-            style={{
-              justifyContent: 'flex-start',
-              height: Dimensions.get('window').height / 2,
-              width: Dimensions.get('window').width,
-              backgroundColor: 'tomato',
-            }}
-            ref={scrollView => {
-              this._rightView = scrollView;
-            }}
-            scrollEventThrottle={16}
-            onScroll={e => {
-              if (!this.rigthIsScrolling) {
-                this.leftIsScrolling = true;
-                var scrollY = e.nativeEvent.contentOffset.y;
-                this._leftView.scrollTo({ y: scrollY });
-              }
-              this.rigthIsScrolling = true;
-            }}>
-            <SquareFavorites numRows={GLOBAL.num} color1={'tomato'} />
-          </ScrollView>
-        </View>
-      </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  btnContainer: {
+  container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    alignSelf: 'stretch',
-    borderRadius: 10,
+    marginTop: 15,
+    marginHorizontal: 16,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    minWidth: 100,
+    marginTop: 20,
+    marginBottom: 20,
+    marginHorizontal: 20,
+    paddingHorizantal: 10,
+    paddingVertical: 5,
+    borderRadius: 3,
   },
 });
