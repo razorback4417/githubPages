f40a322236b24134742d6e3c43b9c0f28c9a6d9a
razorback4417 <razorback4417@users.noreply.github.com>
2020-12-12 23:40:54 -0500

    automated commit by submit50

---
+++ hsConnect/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("register", views.register, name="register"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+               
+    path("becomeTutor", views.becomeTutor, name="becomeTutor"),
+    path("findTutor", views.findTutor, name="findTutor")
+]
